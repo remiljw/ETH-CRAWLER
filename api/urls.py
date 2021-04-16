8c91d37ea40bf2ce1d2458b3dca93ab71a6ec4d2
Michael Ajanaku <remiljw@gmail.com>
2021-04-16 09:14:07 +0100

    - Minor fixes

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from .views import crypto_articles, ethereum_price, ethereum_wallet_value
 urlpatterns = [
     path('crypto-news', crypto_articles, name='news'),
     path('eth-price', ethereum_price, name='price'),
-    path('wallet/<str:address>', ethereum_wallet_value, name='value'),
+    path('wallet-value/<str:address>', ethereum_wallet_value, name='value'),
 ]
