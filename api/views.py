0d98e19e2cf164f578ffb677b2618e507982df90
Michael Ajanaku <remiljw@gmail.com>
2021-04-16 09:09:21 +0100

    - Made Minor Fixes

--- api/views.py
+++ api/views.py
@@ -16,8 +16,5 @@ def ethereum_price(request):
     return Response({'price' :eth_price()})
 
 @api_view(['GET'])
-def ethereum_wallet_value(request):
-    if 'address' not in request.data:
-        return Response({'message': 'Provide an Ethereum Wallet Address'}, status=status.HTTP_400_BAD_REQUEST)
-    address = request.data['address']
-    return Response({'value' : address_value(address)})
+def ethereum_wallet_value(request, address):
+    return Response({'value' : address_value(address)}) 
